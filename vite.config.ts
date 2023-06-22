6aa5e0c103b6cc234c594b1fa2e38967d499cda2
sualee <rkttndk@naver.com>
2023-06-22 18:51:17 +0900

    [feat] component using another component called by host app doesn't work in vite-module-federation-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import federation from "@originjs/vite-plugin-federation";
 import topLevelAwait from 'vite-plugin-top-level-await'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-const dependencies = require("./package.json").dependencies;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "http://localhost:5174",
@@ -18,9 +18,11 @@ export default defineConfig({
       filename: 'remoteEntry.js',
       exposes: {
         './RepoContent': './src/App.vue',
+        './HomeView':'./src/views/HomeView.vue',
+        './AboutView':'./src/views/AboutView.vue',
         './RepoRoutes':'./src/router/index.ts'
       },
-      shared: [{...dependencies },'vue','pinia',"vue-router"],
+      shared: ['vue','pinia',"vue-router"],
 
     }),
     topLevelAwait({
